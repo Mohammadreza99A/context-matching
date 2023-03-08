ee81029bf1fe03e80b40db300ccdf4bc330c3223
Mohammadreza Amini <mohammadreza99a@yahoo.com>
2023-03-08 15:35:14 +0100

    fixed bug in showing observation in debug representation

--- scripts/debug_context.py
+++ scripts/debug_context.py
@@ -1,9 +1,6 @@
 from sys import argv
 import pandas as pd
-import pandas as pd
-import plotly.graph_objects as go
 import plotly.express as px
-import numpy as np
 
 
 def generate_plot(history_file):
@@ -14,6 +11,7 @@ def generate_plot(history_file):
     # melt columns p_0 to p_100 into a single column 'p'
     df_melted = old_df.melt(id_vars=['id'], value_vars=[
         f'p_{i}' for i in range(0, 101)], var_name='y', value_name='ctx')
+
     # set y as integer instead of string
     df_melted['y'] = df_melted['y'].str.extract('(\d+)').astype(int)
 
@@ -21,19 +19,25 @@ def generate_plot(history_file):
     df = pd.DataFrame(
         {'ctx': df_melted['ctx'], 'x': df_melted['id'], 'y': df_melted['y']})
 
+    # df.to_csv("test.csv")
+
     print(df)
 
-    df['range'] = df['x'].apply(lambda x: x//100)
+    df['range'] = df['x'].apply(lambda x: x//101)
 
-    fig = px.scatter(df, x='x', y='y', color='ctx', animation_frame='range')
+    fig = px.scatter(df, x='x', y='y', color='ctx', animation_frame='range',
+                     labels={
+                         "x": "Observations",
+                         "y": "Particles",
+                     },)
 
     # Set the range of the x-axis to the range of x values for each slider step
-    fig.update_xaxes(range=[-1, 101])
+    fig.update_xaxes(range=[-1, 102])
 
     # Update the range of the x-axis for each subsequent slider step
-    for i in range(1, df['range'].max() + 1):
-        x_min = i * 100
-        x_max = (i + 1) * 100 - 1
+    for i in range(0, df['range'].max() + 1):
+        x_min = i * 101
+        x_max = (i + 1) * 101 - 1
         fig['frames'][i]['layout']['xaxis']['range'] = [x_min - 1, x_max + 1]
 
     fig.show()
